7aa3ddad916faa8001e5fcb5cc6e9d596da3a254
Dan <njdan.5691@gmail.com>
2019-01-23 07:23:42 -0500

    Create idev-pkg.el

---
+++ idev-pkg.el
@@ -0,0 +1,3 @@
+;;; -*- no-byte-compile: t -*-                                                                     
+(define-package "idev" "0.1" "INC development elisp" '((cl-lib "0.6.1")) :authors '(("Daniel" . "d\
+nuzzo@lightriver.com")) :maintainer '("Daniel" . "dnuzzo@lightriver.com"))
